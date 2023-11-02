589e48f10c0eb32b09813e0b8b1af7e4105d90f4
Wilson Chu <wilson1.chu@gmail.com>
2023-11-02 02:23:45 -0400

    used props destructuring

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,18 +4,18 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   const { username, imageSource, id, location, profile } = props;
-  
+
   return (
     <article className="photo-list__item">
       <PhotoFavButton />
-      <img className="photo-list__image" src={props.imageSource} />
+      <img className="photo-list__image" src={imageSource} />
 
       <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={props.profile} />
+        <img className="photo-list__user-profile" src={profile} />
         <div className="photo-list__user-info">
-          <p className="photo-list__user-info">{props.username} </p>
+          <p className="photo-list__user-info">{username} </p>
           <p className="photo-list__user-info photo-list__user-location">
-            {props.location.city}, {props.location.country}
+            {location.city}, {location.country}
           </p>
         </div>
       </div>
