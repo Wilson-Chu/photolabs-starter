b240c4355e5938b8f122308b2627a4a0e7d43d86
Wilson Chu <wilson1.chu@gmail.com>
2023-11-04 21:45:50 -0400

    general layout of modal complete, need to figure out image index props later

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,10 +3,16 @@ import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
 import PhotoList from "../components/PhotoList";
 
+import photos from "mocks/photos";
+import { isLabelWithInternallyDisabledControl } from "@testing-library/user-event/dist/utils";
+
 const PhotoDetailsModal = ({ photo, onClose }) => {
   if (!photo) {
     return null;
   }
+
+  // replace photos[1] with selected photo later...
+  const similarPhotos = Object.values(photos[1].similar_photos);
 
   return (
     <div className="photo-details-modal">
@@ -14,15 +20,19 @@ const PhotoDetailsModal = ({ photo, onClose }) => {
         <img src={closeSymbol} alt="close symbol" />
       </button>
       {/* modal content here */}
-      <div className="photo-details-modal__content">
-        <img src={photo.urls.full} alt={photo.user.username} />
-
-        {/* Display similar photos using PhotoList component */}
-        <div className="photo-details-modal__similar-photos">
-          <h2>Similar Photos</h2>
-          <PhotoList photos={photo.similar_photos} /> {/* Pass similar_photos as photos prop */}
-        </div>
+      <div className="photo-details-modal__images">
+        <img
+          src={photos[1].urls.regular}
+          className="photo-details-modal__image"
+        />
+        <p className="photo-details-modal__header">
+          <strong>Similar Photos</strong>
+        </p>
+        <section className="photo-details-modal__images">
+          <PhotoList photos={similarPhotos} />
+        </section>
       </div>
+      {console.log(photo)} {/* Testing... */}
     </div>
   );
 };
