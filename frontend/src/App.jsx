08bcdabb9ab0df56aa7a2fa61be8448911852ff2
Wilson Chu <wilson1.chu@gmail.com>
2023-11-08 06:03:52 -0500

    removed some comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import "./App.scss";
 import HomeRoute from "routes/HomeRoute";
 import PhotoDetailsModal from "routes/PhotoDetailsModal";
-// import photos from "mocks/photos";
-// import topics from "mocks/topics";
 import useApplicationData from "./hooks/useApplicationData";
 
 const App = () => {
